6d1d035eff16f3e4f0e609f9c1cb1f8f531dc0b2
Eron Alves <eron.alves@rocketmail.com>
2022-11-11 00:26:16 -0300

    Make the main Controller

--- src/main/java/io/eronalves1996/recipebook/RecipesController.java
+++ src/main/java/io/eronalves1996/recipebook/RecipesController.java
@@ -1,5 +1,23 @@
 package io.eronalves1996.recipebook;
 
-public class RecipesController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class RecipesController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/cake")
+	public String cakeRecipe() {
+		return "cake";
+	}
+	
+	@GetMapping("/chicken")
+	public String chickenRecipe() {
+		return "chicken";
+	}
 }
